b935680e18c27375076957260c4d8f86b77484a3
mitsborasolution <52045068+mitsborasolution@users.noreply.github.com>
2022-05-27 15:18:21 +0100

    Sauvegarde avant la dockerisation de Basket.API

---
+++ src/services/basket/Basket.API/Program.cs
@@ -0,0 +1,30 @@
+using Basket.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddStackExchangeRedisCache(option =>
+{
+    option.Configuration = builder.Configuration.GetSection("CacheSettings").GetValue<string>("ConnectionString"); // builder.Configuration.GetValue<string>("CacheSettings.ConnectionString");// "localhost:6379";
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
